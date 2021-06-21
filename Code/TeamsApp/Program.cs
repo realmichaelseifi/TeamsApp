c4f02ff3748f6e78ff6a77a95334d7d42a9bae59
Michael Seifi <michael.seifi@fairwaymc.com>
2021-06-21 16:56:45 -0500

    initialize

---
+++ Code/TeamsApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeamsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
